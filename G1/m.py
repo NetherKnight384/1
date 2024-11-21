8bbe142be509ef1bd4652dccc6985d5b82ee8f17
student <student@6PC3>
2024-11-21 12:55:49 +0300

    Control v1

--- G1/m.py
+++ G1/m.py
@@ -6,105 +6,110 @@ import math, numpy
 
 WIN_W, WIN_H = 1000, 1000
 #-0.001
-GRAW = [0, -0.0001]
+GRAW = [0, 0]
 SIM_SPEED = 0
 square_pos = [0,0]
-square_siz = 0.1
+square_siz = 0.01
+
 
 class particle:
-    def __init__(self, pos, siz, acc, mass):
+    def __init__(self, pos, siz, acc, mass, color, static, lifetime, name):
         self.pos = [pos[0], pos[1]]
         self.acc = [acc[0], acc[1]]
         self.siz = siz
+        self.color = color
         self.mass = mass
-
-        
-    def draw(self):
-        draw_square(self.pos, self.siz)
+        self.static = static
+        self.lifetime = lifetime
+        self.starttime = pygame.time.get_ticks()
+        self.name = name
+     
+    def draw(self,size_w, foll,target_pos):
+        draw_square(self.pos, self.siz, size_w, self.color, foll, target_pos)
     def graw(self,particles):
-        for i in range(len(particles)):
-            cache_p = particles[i]
-            if self.pos != cache_p.pos:
-                cache_d = [cache_p.pos[0] - self.pos[0], cache_p.pos[1] - self.pos[1]]
-                dist = math.sqrt(cache_d[0]**2+cache_d[1]**2)
-                if dist != 0:
-                    norm = [cache_d[0] / dist, cache_d[1] / dist]
-                else:
-                    norm = [0,0]
-                self.acc[0] += norm[0] / (80 * self.mass)
-                self.acc[1] += norm[1] / (80 * self.mass)
-    def tick(self, GRAW):
-        self.acc[0] += GRAW[0]
-        self.acc[1] += GRAW[1]
-        if (self.pos[1] <= -1 + self.siz):
-            self.pos[1] = -1 + self.siz
-            if self.acc[1] < 0:
-                self.acc[1] = 0
-        elif (self.pos[1] >= 1 - self.siz):
-            self.pos[1] = 1 - self.siz
-            if self.acc[1] > 0:
-                self.acc[1] = 0
-        #----------
-        if (self.pos[0] <= -1 + self.siz):
-            self.pos[0] = -1 + self.siz
-            if self.acc[0] < 0:
-                self.acc[0] = 0
-        elif (self.pos[0] >= 1 - self.siz):
-            self.pos[0] = 1 - self.siz
-            if self.acc[0] > 0:
-                self.acc[0] = 0
-            
-
+        if  self.static == False:
+            for i in range(len(particles)):
+                cache_p = particles[i]
+                if self.pos != cache_p.pos and cache_p.static != True:
+                    cache_d = [cache_p.pos[0] - self.pos[0], cache_p.pos[1] - self.pos[1]]
+                    dist = math.sqrt(cache_d[0]**2+cache_d[1]**2)
+                    mass = self.mass / cache_p.mass
+                    if dist != 0:
+                        norm = [cache_d[0] / dist, cache_d[1] / dist]
+                    else:
+                        norm = [0,0]
+                    self.acc[0] += norm[0] / (80 * mass)
+                    self.acc[1] += norm[1] / (80 * mass)
+    def tick(self, GRAW, particles):
+        if  self.static == False:
+            self.acc[0] += GRAW[0]
+            self.acc[1] += GRAW[1]
+        
+        
     def move_tick(self):
-        self.pos[0] += self.acc[0]
-        self.pos[1] += self.acc[1]
+        if  self.static == False:
+            self.pos[0] += self.acc[0]
+            self.pos[1] += self.acc[1]
     
     def colision(self, particles):
-        for i in range(len(particles)):
-            cache_p = particles[i]
-            cache_d = [cache_p.pos[0] - self.pos[0], cache_p.pos[1] - self.pos[1]]
-            dist = math.sqrt(cache_d[0]**2+cache_d[1]**2)
-            if dist != 0:
-                norm = [cache_d[0] / dist, cache_d[1] / dist]
-            else:
-                norm = [0,0]
-            if math.fabs(dist) < cache_p.siz*2:
-                self.pos[0] += cache_p.siz/2 * -norm[0]
-                #self.acc[0] = 0
-                self.pos[1] += cache_p.siz/2 * -norm[1]
-                #self.acc[1] = 0
+        if  self.static == False:
+            for i in range(len(particles)):
+                cache_p = particles[i]
+                cache_d = [cache_p.pos[0] - self.pos[0], cache_p.pos[1] - self.pos[1]]
+                dist = math.sqrt(cache_d[0]**2+cache_d[1]**2)
+                if self.name != cache_p.name and dist <= 5 and cache_p.static != True:
+                    particles.append(particle(self.pos, self.siz, [0,0], 15, [1,0,0], True, 500, len(particles)))
+    
+            
 
 
 
-def draw_square(pos,size):
+def draw_square(pos,size,size_w,color,foll,target_pos):
+    glColor3f(color[0],color[1],color[2])
     glBegin(GL_QUADS)
-    glVertex2f(pos[0] - size, pos[1] - size)
-    glVertex2f(pos[0] + size, pos[1] - size)
-    glVertex2f(pos[0] + size, pos[1] + size)
-    glVertex2f(pos[0] - size, pos[1] + size)
+    if foll == False:
+        glVertex2f(pos[0]/(WIN_W*size_w) - size, pos[1]/(WIN_H*size_w) - size)
+        glVertex2f(pos[0]/(WIN_W*size_w) + size, pos[1]/(WIN_H*size_w) - size)
+        glVertex2f(pos[0]/(WIN_W*size_w) + size, pos[1]/(WIN_H*size_w) + size)
+        glVertex2f(pos[0]/(WIN_W*size_w) - size, pos[1]/(WIN_H*size_w) + size)
+    elif foll == True:
+        glVertex2f((pos[0]-target_pos[0])/(WIN_W*size_w) - size, (pos[1]-target_pos[1])/(WIN_H*size_w) - size)
+        glVertex2f((pos[0]-target_pos[0])/(WIN_W*size_w) + size, (pos[1]-target_pos[1])/(WIN_H*size_w) - size)
+        glVertex2f((pos[0]-target_pos[0])/(WIN_W*size_w) + size, (pos[1]-target_pos[1])/(WIN_H*size_w) + size)
+        glVertex2f((pos[0]-target_pos[0])/(WIN_W*size_w) - size, (pos[1]-target_pos[1])/(WIN_H*size_w) + size)
     glEnd()
 
-def draw_all(particles):
+def draw_all(particles,size_w, foll):
     glClear(GL_COLOR_BUFFER_BIT)
-    draw_square(square_pos, square_siz)
+    draw_square(square_pos, square_siz,size_w, [0,1,0], foll, square_pos)
     for i in range(len(particles)):
         cache = particles[i]
-        cache.draw()
+        cache.draw(size_w,foll,square_pos)
     pygame.display.flip()
 
 
 def sim_all(particles,GRAW):
-    
+    de = []
     for i in range(len(particles)):
         cache = particles[i]
-        #cache.graw(particles)
-        cache.tick(GRAW)
+        if cache.lifetime != "inf":
+            if cache.starttime + cache.lifetime <= pygame.time.get_ticks(): 
+                de.append(i)
+    for i in range(len(de)):
+        particles.pop(de[i]-i)
+    de.clear
+
+    for i in range(len(particles)):
+        cache = particles[i]
+        cache.graw(particles)
+        cache.tick(GRAW,particles)
     for i in range(len(particles)):
         cache = particles[i]
         cache.colision(particles)
     for i in range(len(particles)):
         cache = particles[i]
         cache.move_tick()
+    
 
 
 def main():
@@ -117,9 +122,12 @@ def main():
     glOrtho(-1,1,-1,1,-1,1)
     glMatrixMode(GL_MODELVIEW)
 
+    foll = False
+    size_w = 1
     particles = []
     sim_speedt = 0
     last_spawn = pygame.time.get_ticks()
+    last_butt = pygame.time.get_ticks()
     run = True
     clock = pygame.time.Clock()
 
@@ -129,31 +137,49 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 run = False
-
         key = pygame.key.get_pressed()
+        if key[K_EQUALS]:
+            if size_w > 0.5:
+                size_w -= 0.1
+        if key[K_MINUS]:
+            size_w += 0.1
         if key[K_LEFT]:
-            square_pos[0] -= 0.01
+            square_pos[0] -= 4
         if key[K_RIGHT]:
-            square_pos[0] += 0.01
+            square_pos[0] += 4
         if key[K_UP]:
-            square_pos[1] += 0.01
+            square_pos[1] += 4
         if key[K_DOWN]:
-            square_pos[1] -= 0.01
+            square_pos[1] -= 4
         if key[K_SPACE]:
-            if last_spawn - pygame.time.get_ticks() < -100:
-                particles.append(particle(square_pos, square_siz, [0,0], 4))
+            if last_spawn - pygame.time.get_ticks() < -300:
+                particles.append(particle(square_pos, square_siz, [0,0], 1, [0,0,1], False, "inf", len(particles)))
                 last_spawn = pygame.time.get_ticks()
-
+        if key[K_b]:
+            if last_spawn - pygame.time.get_ticks() < -300:
+                particles.append(particle(square_pos, square_siz, [0,0], 15, [0,1,1], False, "inf", len(particles)))
+                last_spawn = pygame.time.get_ticks()
+        if key[K_c]:
+            if last_spawn - pygame.time.get_ticks() < -300:
+                particles.append(particle(square_pos, square_siz, [0,0], 15, [1,0,1], True, "inf", len(particles)  ))
+                last_spawn = pygame.time.get_ticks()
+        if key[K_f]:
+            if last_butt - pygame.time.get_ticks() < -300:
+                if foll == False:
+                    foll = True
+                else:
+                    foll = False
+                last_butt = pygame.time.get_ticks()
 
         
         
         if sim_speedt < SIM_SPEED:
             sim_speedt += 1
-            draw_all(particles)
+            draw_all(particles,size_w, foll)
             
         elif sim_speedt == SIM_SPEED:
             sim_speedt = 0
-            draw_all(particles)
+            draw_all(particles,size_w,foll)
             sim_all(particles, GRAW)
         clock.tick(60)
         
